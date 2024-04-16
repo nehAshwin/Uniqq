be2a7ef11039cb3ae56ea67a6344217afc0d7284
Neha Ashwin <107515350+nehAshwin@users.noreply.github.com>
2024-04-16 11:18:20 -0700

    Create ll.h

---
+++ ll.h
@@ -0,0 +1,29 @@
+#ifndef _LL
+
+#define _LL
+
+#include "item.h"
+
+#include <stdbool.h>
+
+typedef struct Node Node;
+
+struct Node {
+    item data;
+    Node *next;
+};
+
+typedef struct LL LL;
+struct LL {
+    Node *head;
+    Node *tail;
+};
+
+LL *list_create(void);
+bool list_add(LL *, item *);
+item *list_find(LL *, bool (*)(item *, item *), item *);
+
+void list_destroy(LL **);
+void list_remove(LL *, bool (*)(item *, item *), item *);
+
+#endif
